50907b51e7f3d2a67db5cc6e5957f5e51e0a3ecf
Kevin Sage <ksagedeveloper@gmail.com>
2026-01-06 17:51:15 -0500

    update to url

--- bfp-frontend/vite.config.ts
+++ bfp-frontend/vite.config.ts
@@ -6,21 +6,22 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: { "@": path.resolve(__dirname, "src") },
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+    server: {
+      proxy: {
+        "/api": {
+          // Only use localhost if we aren't in production
+          target: process.env.VITE_API_BASE_URL || "http://localhost:8000",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
